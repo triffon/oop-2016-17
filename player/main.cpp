4b728f61bf6ef5f3bc2b05f4c29903b68e1435d4
Trifon Trifonov <triffon@fmi.uni-sofia.bg>
2017-03-23 11:40:07 +0200

    Примери за Memory leak При липса на деструктор

--- player/main.cpp
+++ player/main.cpp
@@ -39,8 +39,26 @@ void testDestructor() {
 	for(int i = 0; i < 1E8; i++) {
 		Player p("Гандалф Сивия", 45);
 		p.setScore(50);
+		// !!! delete p.getName();
 	}
 
+	for(int i = 0; i < 1E8; i++) {
+		Player* p = new Player[10];
+		delete[] p;
+	}
+
+	Player* pa[10];
+	for(int i = 0; i < 10; i++) {
+		pa[i] = new Player("Орк");
+		pa[i]->print();
+	}
+
+	delete pa[2];
+	delete pa[8];
+
+	for(int i = 0; i < 10; i++)
+		if (i != 2 && i != 8)
+			delete pa[i];
 }
 
 int main() {
